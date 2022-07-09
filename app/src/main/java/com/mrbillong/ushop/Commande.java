d29a9982734ee8a88ea58fa1ccb32a04a80f2af9
TEUGUIA <69282466+LeTeguis@users.noreply.github.com>
2022-07-09 10:36:09 +0100

    design

---
+++ app/src/main/java/com/mrbillong/ushop/Commande.java
@@ -0,0 +1,13 @@
+package com.mrbillong.ushop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Commande extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commande);
+    }
+}
